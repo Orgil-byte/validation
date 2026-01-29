7e43814bfe77329020fe5410d60fb0f19670039f
U.Orgil <ulziitogtohorgil1908@gmail.com>
2026-01-29 10:22:39 +0800

    A

--- src/app/_components/DragImg.jsx
+++ src/app/_components/DragImg.jsx
@@ -1,46 +1,41 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Label = ({ label, error, type, setFormData }) => {
-  const [imgUrl, setImgUrl] = useState(null);
   const [imgPreview, setImgPreview] = useState(null);
-  const [zIndexClass, setZIndexClass] = useState({
+  const [classes, setClasses] = useState({
     index: "z-[-2]",
     class: "",
     x: "",
   });
+  const ref = useRef();
 
-  const newImgClass = {};
-
-  const handleChange = (e) => {
-    const file = e.target.files[0];
-
-    if (file && file.type.startsWith("image/")) {
-      setImgUrl(file);
-      const url = URL.createObjectURL(file);
-      setImgPreview(url);
-      setFormData((prev) => ({ ...prev, image: file }));
+  const isActive = (condition) => {
+    if (condition === true) {
       newImgClass.index = "z-[2]";
       newImgClass.class =
         "absolute top-4 right-4 flex items-center justify-center w-6 h-6 bg-[#202124] rounded-full cursor-pointer z-[3] text-white";
       newImgClass.x = "x";
     } else {
-      setImgUrl(null);
-      setImgPreview(null);
       newImgClass.index = "z-[-2]";
       newImgClass.class = "";
       newImgClass.x = "";
     }
-    setZIndexClass(newImgClass);
+    setClasses(newImgClass);
+  };
+  const newImgClass = {};
+
+  const handleChange = () => {
+    const file = ref.current.files[0];
+    const url = URL.createObjectURL(file);
+    setImgPreview(url);
+    setFormData((prev) => ({ ...prev, image: file }));
+    isActive(true);
   };
 
   const removeImage = () => {
     setFormData((prev) => ({ ...prev, image: "" }));
-    setImgUrl(null);
     setImgPreview(null);
-    newImgClass.index = "z-[-2]";
-    newImgClass.class = "";
-    newImgClass.x = "";
-    setZIndexClass(newImgClass);
+    isActive(false);
   };
 
   return (
@@ -49,8 +44,8 @@ const Label = ({ label, error, type, setFormData }) => {
         {label} <span className="text-[#e14942] text-[16px]">*</span>
       </p>
       <div className="flex flex-col items-center justify-center gap-y-2  bg-gray-100 h-45 border border-gray-300 rounded-md border-solid relative">
-        <div onClick={removeImage} type="button" className={zIndexClass.class}>
-          {zIndexClass.x}
+        <div onClick={removeImage} type="button" className={classes.class}>
+          {classes.x}
         </div>
         <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center">
           <img
@@ -62,6 +57,8 @@ const Label = ({ label, error, type, setFormData }) => {
         <h4 className="text-sm text-center">Browse or Drop Image</h4>
         {!imgPreview && (
           <input
+            accept="image/*"
+            ref={ref}
             type={type}
             className="w-full h-full opacity-0  cursor-pointer absolute"
             onChange={handleChange}
@@ -69,7 +66,7 @@ const Label = ({ label, error, type, setFormData }) => {
         )}
         {imgPreview && (
           <img
-            className={`h-full w-full object-cover absolute ${zIndexClass.index}`}
+            className={`h-full w-full object-cover absolute ${classes.index}`}
             src={imgPreview}
             alt=""
           />
